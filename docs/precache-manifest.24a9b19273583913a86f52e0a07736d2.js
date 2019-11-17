4364c28aa707ad87661fc6eaf88a98ee5f50ead7
Konstantin Melnikov <koc.melnikov.1985@gmail.com>
2019-11-17 18:08:04 +0300

    Added build for github pages

---
+++ docs/precache-manifest.24a9b19273583913a86f52e0a07736d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c63a62fc452df85eeff6795baa0c0152",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9b4cc50508e7b9a4427",
+    "url": "/static/css/main.93c9b551.chunk.css"
+  },
+  {
+    "revision": "97b2574b2ecdc792fe66",
+    "url": "/static/js/2.f8c82fa5.chunk.js"
+  },
+  {
+    "revision": "d9b4cc50508e7b9a4427",
+    "url": "/static/js/main.a76a9054.chunk.js"
+  },
+  {
+    "revision": "e33f5b40372bb027ad83",
+    "url": "/static/js/runtime-main.2bd05861.js"
+  },
+  {
+    "revision": "e1d9747cff5ce63849411a0ae87e0dba",
+    "url": "/static/media/PhotoGallery.e1d9747c.svg"
+  },
+  {
+    "revision": "3ce65b80067c9320acac07fa0e94fdcb",
+    "url": "/static/media/face.3ce65b80.svg"
+  }
+]);
